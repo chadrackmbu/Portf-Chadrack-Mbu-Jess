db547cd603da2de9d23e243a99300f90ef25ae39
Chadrack Mbu Jess <chadrackmbupioka@gmail.com>
2023-08-06 20:03:22 +0100

    integration de js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
-//la recuperation des elements
-const form = document.querySelector("#form");
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const password2 = document.querySelector('#password2');
 
-//Evenements
-form.addEventListener('submit',e=>{
-    e.preventDefault();
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-    form_verify();
-})
-//Fonctions
-function form_verify(){
-  //Obtenir toute les valeurs des inputs
-  const uservalue = username.value.trim(); //Enleve les espaces du debut et la fin
-  const emailvalue = email.value.trim();
-  const pwdvalue = password.value.trim();
-  const pwd2value = password2.value.trim();
-
-  //Username verify
-  if (uservalue === ""){
-    let message ="Username ne peut pas être vide";
-    setError(username,message); //gerer l'erreur
-  } else if(!uservalue.match(/^[a-zA-Z]/)){
-    let message="username doit commencer par une lettre";
-    setError(username,message)  
-
-  }
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
-function setError(elem,message){
-    const formControl = elem.parentElement;
-    const small = formControl.querySelector('small');
 
-    //Ajout du message d'erreur
-    small.innertText = message
 
-    //Ajout de la classe d'erreur
-    formControl.className ="Form-control error";
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll ('header nav a');
+
+window.onscroll=()=> {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let  height = sec.offsetHeight;
+    let id = sec.getAttribute('id')
+
+    if(top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    }
+  });
+
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky',window.scrollY > 100);
+
 }
