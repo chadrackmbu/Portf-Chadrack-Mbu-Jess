fc4c65d3d6f65ba7067dadf3a3a44b027047eb93
Chadrack Mbu Jess <chadrackmbupioka@gmail.com>
2023-08-08 07:59:45 +0100

    changement de photos

--- js/script.js
+++ js/script.js
@@ -11,12 +11,15 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll ('header nav a');
 
-window.onscroll=()=> {
+//scroll sections 
+
+window.onscroll= () => {
+  
   sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 100;
-    let  height = sec.offsetHeight;
-    let id = sec.getAttribute('id')
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
     if(top >= offset && top < offset + height) {
       //active navbar links
@@ -24,13 +27,31 @@ window.onscroll=()=> {
         links.classList.remove('active');
         document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
       });
+      //active sections for animation on scroll
+      sec.classList.add('show-animate');
+
     }
+    //if want to use animate that repeats on scroll use this
+    else {
+      sec.classList.remove('show-animate');
+    }
+
   });
 
   let header = document.querySelector('header');
 
   header.classList.toggle('sticky',window.scrollY > 100);
 
+  //remove toogle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+  //animation footer on scroll
+  let footer = document.querySelector('footer');
+
+  footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
+  
 
 
 }
